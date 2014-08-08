f442307a1a6cd94b17bc75de200282efdaf8f536
Florian Albertz <flo.albertz@gmail.com>
2014-08-08 19:45:19 +0200

    Use parameterized routes for our docs

--- assets/js/views/docs.js
+++ assets/js/views/docs.js
@@ -1,17 +1,4 @@
 var View = require('../view/view.js');
-var view = new View('/docs');
-
-view.attach('click', '.doc', function() {
-	var name = this.getAttribute('doc-id');
-	view.data.docs.forEach(function(el) {
-		if(el.name === name) {
-			el.selected = true;
-			view.data.selected = el;
-		} else {
-			el.selected = false;
-		}
-	});
-	view.render();
-});
+var view = new View('/docs/:doc');
 
 module.exports = view;
--- views/docs/data.js
+++ views/docs/data.js
@@ -2,21 +2,26 @@ var Promise = require('bluebird');
 var fs = Promise.promisifyAll(require('fs'));
 var path = require('path');
 
-var docs = fs.readdirAsync('docs').map(function(filename) {
+var docs = fs.readdirAsync('docs').reduce(function(sum, filename) {
 	return fs.readFileAsync(path.join('docs', filename), 'utf8').then(function(content) {
-		return {
-			name: filename.split('.')[0],
-			content: content
-		}
+		sum[filename.split('.')[0]] = content;
+		return sum;
 	});
-}).then(function(docs) {
-	docs[0].selected = true;
-	return {
-		docs: docs,
-		selected: docs[0]
-	};
-});
+}, {});
 
-module.exports = function *() {
-	return yield docs;
+module.exports = function *(doc) {
+	var data = yield docs;
+	if(!doc) {
+		this.redirect(this.path + '/' + Object.keys(data)[0]);
+	} else {
+		return {
+			files: Object.keys(data).map(function(el) {
+				return {
+					name: el,
+					selected: el === doc
+				}
+			}),
+			contents: data[doc]
+		};
+	}
 };
