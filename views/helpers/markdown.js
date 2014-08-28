d6d52543a3097015ad6b81629b01efbd89200fb6
Florian Albertz <flo.albertz@gmail.com>
2014-08-28 21:46:12 +0200

    Some weird markdown rendering problems

--- views/helpers/markdown.js
+++ views/helpers/markdown.js
@@ -6,12 +6,12 @@ marked.setOptions({
 	highlight: function(code) {
 		return highlight.highlightAuto(code).value;
 	}
-})
+});
 
 module.exports = function(context) {
 	var arr = context.fn(context.data.root).split('\n');
-	var moved = /^([ \t]*)/.exec(arr[1])[1].length;
-	arr[1] = arr[1].substr(moved);
+	var moved = /^([ \t]*)/.exec(arr[0])[1].length;
+	arr[0] = arr[0].substr(moved);
 
 	return new hbs.SafeString(marked(arr.join('\n')));
 };
