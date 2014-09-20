4e2134be3fb9610be112b572f9f796f188032588
Florian Albertz <flo.albertz@gmail.com>
2014-09-20 10:30:54 +0200

    Create PORT global var

--- server/setup.js
+++ server/setup.js
@@ -1,10 +1,13 @@
 var _ = require('lodash');
+var dir = require('require-directory');
+
 //Enable sensible stacktraces for promises
 process.env.BLUEBIRD_DEBUG = 1;
 
 //Makes config, services, logger and environment available globally
 module.exports = function() {
 	global.NAME = require('../package.json').name;
+	global.PORT = process.env.PORT || 3000;
 	global.BASEPATH = process.env.BASEPATH || '';
 	global.DEV = process.env.NODE_ENV !== 'production';
 	global.logger = require('bunyan').createLogger({
@@ -12,8 +15,8 @@ module.exports = function() {
 		level: DEV ? 'trace' : 'info',
 		src: DEV
 	});
-	global.config = require('require-directory')(module, '../config');
-	global.services = _(require('require-directory')(module, '../services')).transform(function(obj, el, key) {
+	global.config = dir(module, '../config');
+	global.services = _(dir(module, '../services')).transform(function(obj, el, key) {
 		obj[key] = el();
 	}).value();
 };
